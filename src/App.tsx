df248ba5235dd2f16e896514e107f290fd38977f
Pedroo <pedrohenriquemoalive@gmail.com>
2024-02-08 11:41:25 -0300

    Armazenando notas no  localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,28 @@ import { AddNoteCard } from './components/AddNoteCard'
 import { NoteCard } from './components/NoteCard'
 type Note = {
   id: string,
-  date:Date,
+  date: Date,
   content: string
 }
 export const App = () => {
-  const [notes, SetNotes] = useState<Note[]>([])
-  const onNoteCreated = (content : string) => {
+  const [notes, SetNotes] = useState<Note[]>(() => {
+    const notesOnStorage = localStorage.getItem('notes')
+
+    if (notesOnStorage) return JSON.parse(notesOnStorage)
+
+    return []
+  })
+  const onNoteCreated = (content: string) => {
     let newNote = {
       id: crypto.randomUUID(),
       date: new Date(),
       content
     }
-    SetNotes([newNote, ...notes])
+    const notesArray = [newNote, ...notes]
+
+    SetNotes(notesArray)
+
+    localStorage.setItem('notes' , JSON.stringify(notesArray))
   }
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6">
@@ -28,7 +38,7 @@ export const App = () => {
       </form>
       <div className='h-px bg-slate-700' />
       <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
-        <AddNoteCard onNoteCreated={onNoteCreated}/>
+        <AddNoteCard onNoteCreated={onNoteCreated} />
         {notes.map(note => {
           return <NoteCard key={note.id} note={note} />
         })}
